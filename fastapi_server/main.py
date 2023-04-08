e854c3d87ba0b61acb075fa87326d5bab6ecdfc5
BasicOverflow <peter.filip@uconn.edu>
2023-04-08 11:29:51 -0700

    yeah

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -1,15 +1,9 @@
-import threading
+import httpx
 from fastapi import FastAPI
 import requests
 
 app = FastAPI()
 
-# def yeah():
-#     callback_url = "http://microservice1:81/test"
-#     response = requests.get(callback_url)
-#     print(response.text)  
-
-# threading.Thread(target=yeah).start()
 
 @app.get("/")
 def read_root():
@@ -20,11 +14,12 @@ def read_root():
 async def callback():
     '''route that calls function to make call to other microservice'''
     # make a callback to another server
-    callback_url = "http://microservice1:81/test" 
-    response = requests.get(callback_url)
+    async with httpx.AsyncClient() as client:
+        callback_url = "http://microservice1:81/test" 
+        data = await client.get(callback_url)
     
     # handle the response from the other server
     # ...
     
-    return {"data from microservice": response.text}
+    return {"data from microservice": data}
 
