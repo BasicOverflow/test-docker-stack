8515e1820ea14ee24b5984f26680cb1093bb19af
BasicOverflow <peter.filip@uconn.edu>
2023-04-09 16:51:21 -0700

    hhh

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -16,12 +16,12 @@ async def callback():
     # make a callback to another server
     async with httpx.AsyncClient() as client:
         callback_url = "http://microservice1:81/test" 
-        data = await str(client.get(callback_url))
+        data = await client.get(callback_url)
     
     # handle the response from the other server
     # ...
     
-    return data
+    return str(data)
 
 
 
